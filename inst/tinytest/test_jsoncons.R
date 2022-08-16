31704da0279da9858cda584e3183a08e5e986ad7
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-08-16 13:44:05 -0400

    add tinytest and unit tests

---
+++ inst/tinytest/test_jsoncons.R
@@ -0,0 +1,37 @@
+library(jsonlite)
+
+expect_true(
+    is.character(version())
+)
+
+json <- '{
+  "locations": [
+    {"name": "Seattle", "state": "WA"},
+    {"name": "New York", "state": "NY"},
+    {"name": "Bellevue", "state": "WA"},
+    {"name": "Olympia", "state": "WA"}
+  ]
+ }'
+
+expect_identical(
+    jsonpath(json, "$..name") |> fromJSON(),
+    c("Seattle", "New York", "Bellevue", "Olympia")
+)
+
+expect_identical(
+    jmespath(json, "locations[?state == 'WA'].name | sort(@)") |>
+        fromJSON(),
+    c("Bellevue", "Olympia", "Seattle")
+)
+
+datalist <- fromJSON(json)
+expect_identical(
+    jmespath(datalist, "locations[?state == 'WA'].name | sort(@)") |>
+        fromJSON(),
+    c("Bellevue", "Olympia", "Seattle")
+)
+
+expect_identical(
+    jsonpath(datalist, "$..name") |> fromJSON(),
+    c("Seattle", "New York", "Bellevue", "Olympia")
+)
