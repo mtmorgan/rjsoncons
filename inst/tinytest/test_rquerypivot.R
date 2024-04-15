8eb5f9fd32ec1a04ab5601ddb5c2656430dbd04b
Martin Morgan <Martin.Morgan@RoswellPark.org>
2024-04-15 13:10:20 -0400

    better handle j_pivot() when keys differ between objects

--- inst/tinytest/test_rquerypivot.R
+++ inst/tinytest/test_rquerypivot.R
@@ -137,3 +137,34 @@ expect_identical(
 )
 
 expect_error(j_pivot(json, "locations[0].name"))
+
+## j_pivot ndjson
+
+ndjson_con <- tempfile(fileext = ".ndjson")
+
+json <- '[{"a": 1}, {"a": 2, "b": 3}]' # additional key in second object
+ndjson_vector <- c('{"a": 1}', '{"a": 2, "b": 3}')
+writeLines(ndjson_vector, ndjson_con)
+expected <- '{"a":[1,2],"b":[null,3]}'
+expect_identical(j_pivot(json), expected)
+expect_identical(j_pivot(ndjson_vector), expected)
+expect_identical(j_pivot(ndjson_con), expected)
+
+json <- '[{"a": 2, "b": 3}, {"a": 1}]' # fewer keys in second object
+ndjson_vector <- c('{"a": 2, "b": 3}', '{"a": 1}')
+writeLines(ndjson_vector, ndjson_con)
+expected <- '{"a":[2,1],"b":[3,null]}'
+expect_identical(j_pivot(json), expected)
+expect_identical(j_pivot(ndjson_vector), expected)
+expect_identical(j_pivot(ndjson_con), expected)
+
+json <- '[{"a": 1},{"a": 2},{"a": 3, "b": 4},{"c": 5}]' # complex
+ndjson_vector <- c('{"a": 1}', '{"a": 2}', '{"a": 3, "b": 4}', '{"c": 5}')
+writeLines(ndjson_vector, ndjson_con)
+expected <- '{"a":[1,2,3,null],"b":[null,null,4,null],"c":[null,null,null,5]}'
+expect_identical(j_pivot(json), expected)
+expect_identical(j_pivot(ndjson_vector), expected)
+expect_identical(j_pivot(ndjson_con), expected)
+
+expect_identical(j_data_type(json), "json") # FIXME: can we be smarter
+expect_identical(j_data_type(ndjson_vector), "ndjson")
